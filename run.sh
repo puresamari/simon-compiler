0a7bb210b72066c90be5bab346ac79cc0f981ede
Simon the Grey <puresamari@gmail.com>
2023-12-14 10:35:13 +0100

    Initial commit

---
+++ run.sh
@@ -0,0 +1,16 @@
+rm -rf ./dist
+mkdir ./dist
+
+clang ./compiler/compiler.c -o ./dist/compiler
+
+./dist/compiler ./test/test.simon ./dist/test-out-binary
+
+# Assemble the code
+as -arch arm64 -o ./dist/test-out-binary.o ./dist/test-out-binary.s
+
+# Link the code
+ld -arch arm64 -e _start -o  ./dist/test-out-binary ./dist/test-out-binary.o -lSystem -syslibroot `xcrun -sdk macosx --show-sdk-path` -e _start -arch arm64
+
+echo "\n\nExecuting....\n"
+./dist/test-out-binary
+# cat ./dist/test-out-binary.s
